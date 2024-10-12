5934aae0df12f970162100360ce192f01ab0492d
Thomas Diggs <tbd@mac.lan>
2024-10-12 16:33:14 -0600

    Initial commit

--- obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeeaadd6ed5760bfc6194c3c92654e6913f4ded4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
